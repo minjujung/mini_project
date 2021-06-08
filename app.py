dbdfc95493a481f7b3951102a5d96f87f098108b
goplanit <goplanit19@gmail.com>
2021-06-08 20:25:07 +0900

    mainpage upload

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ from flask.json import jsonify
 
 app = Flask(__name__)
 from pymongo import MongoClient
-client = MongoClient('54.180.114.126', 27017, username="test", password="test")
-db = client.dbsparta_plus_week2
-# client = MongoClient('localhost', 27017)
-# db = client.team35_db
+client = MongoClient('localhost', 27017)
+db = client.team35_db
 
 @app.route('/')
 def main():
@@ -15,7 +13,7 @@ def main():
 
 @app.route('/diary')
 def listing():
-    diaries = list(db.diary.find({}, {'_id': False}))
+    diaries = list(db.articles.find({}, {'_id': False}))
     return jsonify({'all_articles': diaries})
 
 if __name__ == '__main__':
